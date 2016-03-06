67e9a0076aafe9c04dcaf8357885e3cad94c5286
Thomas Stache <dev@thomasstache.de>
2016-03-06 20:06:45 +0100

    Add MapView using Mapbox API

--- app/src/main/java/de/thomasstache/parkbote/MapActivity.java
+++ app/src/main/java/de/thomasstache/parkbote/MapActivity.java
@@ -3,10 +3,21 @@ package de.thomasstache.parkbote;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdate;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
 
 public class MapActivity extends AppCompatActivity
 {
 	private static final String TAG = "ParkboteApp";
+
+	private MapView mapView = null;
+	private CameraUpdate homeCamera;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -15,5 +26,69 @@ public class MapActivity extends AppCompatActivity
 		setContentView(R.layout.activity_map);
 
 		Log.i(TAG, "Running onCreate()");
+
+		setupMapView();
+		setInitialCamera();
+		mapView.moveCamera(homeCamera);
+
+		mapView.onCreate(savedInstanceState);
+	}
+
+	private void setupMapView()
+	{
+		mapView = (MapView) findViewById(R.id.map);
+		mapView.setStyleUrl(Style.MAPBOX_STREETS);
+	}
+
+	private void setInitialCamera()
+	{
+		CameraPosition cam = new CameraPosition.Builder()
+				.target(new LatLng(51.063, 13.746))
+				.zoom(14)
+				.build();
+
+		homeCamera = CameraUpdateFactory.newCameraPosition(cam);
+	}
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		mapView.onStart();
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		mapView.onStop();
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		mapView.onPause();
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		mapView.onResume();
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
 	}
 }
