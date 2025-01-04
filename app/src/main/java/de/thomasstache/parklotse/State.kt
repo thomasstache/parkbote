57ef518abb023a37ae426292fedfe6185f08ee66
Thomas Stache <dev@thomasstache.de>
2025-01-04 14:21:26 +0100

    Convert MapActivity to Kotlin - adjustments and Ktlint in `State`

--- app/src/main/java/de/thomasstache/parklotse/State.kt
+++ app/src/main/java/de/thomasstache/parklotse/State.kt
@@ -1,38 +1,48 @@
 package de.thomasstache.parklotse
 
 import android.content.SharedPreferences
-
+import androidx.core.content.edit
 import com.mapbox.mapboxsdk.geometry.LatLng
+import java.lang.Double.parseDouble
 
 /**
  * The whole application state.
  */
 class State {
-
     /**
-     * Indicates whether we are currently parked, and { latLng} has a valid value.
+     * Indicates whether we are currently parked, and [latLng] has a valid value.
      */
     var isParked: Boolean = false
 
     // default location at Dresden Albertplatz
     var latLng: LatLng? = LatLng(51.063, 13.746)
 
+    fun saveToPrefs(preferences: SharedPreferences) {
+        val latitude: String = latLng?.latitude?.toString() ?: ""
+        val longitude: String = latLng?.longitude?.toString() ?: ""
+
+        preferences.edit {
+            putBoolean(PREF_PARKED, isParked)
+            putString(PREF_LOCATION_LAT, latitude)
+            putString(PREF_LOCATION_LON, longitude)
+        }
+    }
+
     companion object {
         private const val PREF_PARKED = "isParked"
         private const val PREF_LOCATION_LON = "parkedLocationLongitude"
         private const val PREF_LOCATION_LAT = "parkedLocationLatitude"
 
-        @JvmStatic
         fun createFromPrefs(preferences: SharedPreferences): State {
             val state = State()
 
             state.isParked = preferences.getBoolean(PREF_PARKED, false)
             if (state.isParked) {
                 // LatLng is only valid if we were parked
-                val latitude = preferences.getString(PREF_LOCATION_LAT, "")
-                val longitude = preferences.getString(PREF_LOCATION_LON, "")
+                val latitude = preferences.getString(PREF_LOCATION_LAT, "")!!
+                val longitude = preferences.getString(PREF_LOCATION_LON, "")!!
 
-                if (!latitude!!.isEmpty() && !longitude!!.isEmpty()) {
+                if (latitude.isNotEmpty() && longitude.isNotEmpty()) {
                     state.latLng = parseLatLng(latitude, longitude)
                 }
             }
@@ -40,31 +50,12 @@ class State {
             return state
         }
 
-        @JvmStatic
-        fun saveToPrefs(state: State, preferences: SharedPreferences): Boolean {
-            val editor = preferences.edit()
-
-            val latitude: String
-            val longitude: String
-
-            if (state.latLng != null) {
-                latitude = state.latLng!!.latitude.toString()
-                longitude = state.latLng!!.longitude.toString()
-            } else {
-                latitude = ""
-                longitude = ""
-            }
-
-            editor.putBoolean(PREF_PARKED, state.isParked)
-                    .putString(PREF_LOCATION_LAT, latitude)
-                    .putString(PREF_LOCATION_LON, longitude)
-
-            return editor.commit()
-        }
-
-        private fun parseLatLng(latitude: String, longitude: String): LatLng {
-            val lat = java.lang.Double.parseDouble(latitude)
-            val lon = java.lang.Double.parseDouble(longitude)
+        private fun parseLatLng(
+            latitude: String,
+            longitude: String,
+        ): LatLng {
+            val lat = parseDouble(latitude)
+            val lon = parseDouble(longitude)
 
             // TODO add error checking
 
